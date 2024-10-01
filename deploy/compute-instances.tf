6229a0b698366f767f41dc75ac1eabd880bf2ee4
Leonid Grishenkov <grishenkovleonid@gmail.com>
2024-10-01 10:23:31 +0300

    feat: Add terraform config for targat postgres database

--- deploy/compute-instances.tf
+++ deploy/compute-instances.tf
@@ -49,45 +49,49 @@ data "yandex_compute_instance" "vm-source" {
   instance_id = yandex_compute_instance.vm-source.id
 }
 
-# resource "yandex_compute_instance" "vm-target" {
-#   name        = "postgresql-target"
-#   hostname    = "postgresql-target"
-#   platform_id = "standard-v3"
-#   zone        = var.zone
-#   folder_id   = var.folder_id
+resource "yandex_compute_instance" "vm-target" {
+  name        = "postgresql-target"
+  hostname    = "postgresql-target"
+  platform_id = "standard-v3"
+  zone        = var.zone
+  folder_id   = var.folder_id
 
-#   resources {
-#     cores         = 2
-#     memory        = 2
-#     core_fraction = 100
-#   }
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 100
+  }
 
-#   boot_disk {
-#     initialize_params {
-#       image_id = var.image_id
-#       type     = "network-hdd"
-#       size     = 20
-#     }
-#     auto_delete = true
-#   }
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+      type     = "network-hdd"
+      size     = 20
+    }
+    auto_delete = true
+  }
 
-#   network_interface {
-#     subnet_id          = yandex_vpc_subnet.subnet-a.id
-#     security_group_ids = [yandex_vpc_security_group.sg.id]
-#     nat                = true
-#     ipv4               = true
-#   }
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.subnet-a.id
+    security_group_ids = [yandex_vpc_security_group.sg.id]
+    nat                = true
+    ipv4               = true
+  }
 
-#   metadata = {
-#     user-data = templatefile("./cloud-init.yaml",
-#       {
-#         ssh_key_path      = file("${local.ssh_key_path}"),
-#         username          = local.username,
-#     })
-#   }
-# }
+  metadata = {
+    user-data = templatefile("./cloud-init.yaml",
+      {
+        ssh_key_path      = file("${local.ssh_key_path}"),
+        username          = local.username,
+        iam_token         = var.iam_token
+        postgres_user     = var.postgres_user,
+        postgres_password = var.postgres_password,
+        postgres_image    = var.postgres_image,
+    })
+  }
+}
 
 
-# data "yandex_compute_instance" "vm-target" {
-#   instance_id = yandex_compute_instance.vm-target.id
-# }
+data "yandex_compute_instance" "vm-target" {
+  instance_id = yandex_compute_instance.vm-target.id
+}
--- deploy/output.tf
+++ deploy/output.tf
@@ -1,7 +1,13 @@
 output "vm-source-external-ip" {
   value = data.yandex_compute_instance.vm-source.network_interface.0.nat_ip_address
 }
+output "vm-source-internal-ip" {
+  value = data.yandex_compute_instance.vm-source.network_interface.0.ip_address
+}
 
-# output "vm-target-external-ip" {
-#   value = data.yandex_compute_instance.vm-target.network_interface.0.nat_ip_address
-# }
+output "vm-target-external-ip" {
+  value = data.yandex_compute_instance.vm-target.network_interface.0.nat_ip_address
+}
+output "vm-target-internal-ip" {
+  value = data.yandex_compute_instance.vm-target.network_interface.0.ip_address
+}
